db7c169520f70d8b914c2ebbfd1b0e95696f6141
guilhermealmeida123 <105080670+guilhermealmeida123@users.noreply.github.com>
2024-06-04 12:11:31 -0300

    ok

---
+++ main.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPergunta = document.querySelector(".caixa-principal")
+const caixaalternativa = document.querySelector(".caixa-alternativa")
+const caixaresultado = document.querySelector(".caixa-resultado")
+const caixaresultado = document.querySelector(".caixa-resultado")
+
+const pergunta = [
+    { //primeiro objeto da lista de perguntas
+    enunciado: "pergunta1",
+    alternativas: ["alternativa1", "alternativa2"]
+    }
+    {// segundo objeto da lista de perguntas
+        enunciado: "pergunta2",
+        alternativas: ["alternativa1","alternativa2"]
+    }
+]
